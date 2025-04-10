36a120b2fd6e0be22f25992fd09ec1ac51218dce
Mohammed Shamil <kmshamil123@gmail.com>
2025-04-10 18:49:33 +0400

    add

--- backend/src/controlller/auth-controller.ts
+++ backend/src/controlller/auth-controller.ts
@@ -1,19 +1,23 @@
 import { Request, Response, NextFunction } from "express";
+import * as bcrypt from "bcrypt";
 
 import { ResponseHandler } from "../components/response-handler/response-handler";
-import { AuthService } from "../services/auth/auth-service";
+import { comparePasswords, handleUserExistence } from "../utils/helper/auth/auth-functions";
+import { UserService } from "../services/user/user-service";
+import { generateJwt } from "../utils/helper/jwt/jwt-functions";
 
 export const AuthController = {
   async registerUser(req: Request, res: Response, next: NextFunction) {
     try {
       const { username, password } = req.body;
-
-      const newUser = await AuthService.register(username, password, res);
+      await handleUserExistence({ username, throwUserExistsError: true, res });
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const newUser = await UserService.createUser({ username, password: hashedPassword });
 
       ResponseHandler.success({
         res,
         statusCode: 201,
-        data: { username: newUser.username },
+        data: { username: newUser!.username },
         message: "User registered successfully",
       });
     } catch (error) {
@@ -24,14 +28,26 @@ export const AuthController = {
   async loginUser(req: Request, res: Response, next: NextFunction) {
     try {
       const { username, password } = req.body;
+      await handleUserExistence({ username, throwNoUserExistsError: true, res });
 
-      const { token, username: loggedInUsername } = await AuthService.login(username, password, res);
+      const user = await UserService.findUserByUsername({ username });
+
+      const isPasswordValid = await comparePasswords({
+        plainPassword: password,
+        hashedPassword: user!.password,
+      });
+
+      if (!isPasswordValid) {
+        throw new Error("Invalid credentials");
+      }
+
+      const token = generateJwt(user!);
 
       ResponseHandler.success({
         res,
         statusCode: 200,
         data: {
-          username: loggedInUsername,
+          username: user!.username,
           token,
         },
         message: "Login successful",
--- backend/src/services/user/user-service.ts
+++ backend/src/services/user/user-service.ts
@@ -1,13 +1,23 @@
 import { UserModel } from "../../models/user-schema";
+import { handleMongooseErrors } from "../../utils/helper/mongodb/mongo-functions";
 
 export const UserService = {
-  async getUserByUsername({ username }: { username: string }) {
+  async findUserByUsername({ username }: { username: string }) {
     const user = await UserModel.findOne({ username });
     return user;
   },
 
-  async getUserById({ _id }: { _id: string }) {
+  async findUserById({ _id }: { _id: string }) {
     const user = await UserModel.findById({ _id });
     return user;
+  },
+  async createUser({ username, password }: { username: string; password: string }) {
+    try {
+      const newUser = new UserModel({ username, password });
+      await newUser.save();
+      return newUser;
+    } catch (error) {
+      handleMongooseErrors(error);
+    }
   },
 };
