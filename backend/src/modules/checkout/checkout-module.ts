fd00ce25fb10a0a0156626bd77c20154a5970a05
Mohammed Shamil <kmshamil123@gmail.com>
2025-04-16 20:28:52 +0400

    add

--- backend/src/modules/checkout/checkout-module.ts
+++ backend/src/modules/checkout/checkout-module.ts
@@ -4,7 +4,7 @@ import stripeRoutes from "./routes/stripe-routes";
 export const checkoutModule = {
   routes: {
     v1: stripeRoutes,
-    v2: checkoutRoutes,
+    // v2: checkoutRoutes,
   },
 
   controllers: {},
